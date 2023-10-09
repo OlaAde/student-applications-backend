5e86ec98d5885d37907f8fd3c0be4ea1a7c99a38
Eliot Fagnon <efagnon2002@gmail.com>
2023-10-09 16:32:01 +0100

    feat: Initial commit

---
+++ src/test/java/com/eliotfgn/studentapplicationbackend/StudentApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliotfgn.studentapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
